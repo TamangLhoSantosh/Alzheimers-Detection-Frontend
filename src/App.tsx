9ea3247ebf618c0ea0ca481e08f54e662c05cc76
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-21 22:03:03 +0545

    message component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Login from "./components/Login";
 import CreateAccount from "./components/CreateAccount";
+import MessageComponent from "./components/MessageComponent";
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <CreateAccount />
+      {/* <CreateAccount /> */}
+      <MessageComponent
+        title={"Sdfs"}
+        message={"sadfsa"}
+        onClose={() => {
+          console.log(new Date());
+        }}
+      />
     </>
   );
 };
