6c7cfdb22451ae34b0462a061216aa5d01288462
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-25 19:30:19 +0545

    basic navbar

--- src/components/generic/NavbarComponent.tsx
+++ src/components/generic/NavbarComponent.tsx
@@ -1,5 +1,32 @@
+import { Box, Typography } from "@mui/material";
+
 const NavbarComponent = () => {
-  return <div>NavbarComponent</div>;
+  return (
+    <Box
+      position="fixed"
+      top="0"
+      display="grid"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100%"
+      zIndex="99"
+    >
+      <Box gridColumn="1" p="20px">
+        Logo
+      </Box>
+      <Box
+        gridColumn="2"
+        p="20px"
+        display="flex"
+        justifyContent="space-evenly"
+        gap="30px"
+      >
+        <Typography>Hospitals</Typography>
+        <Typography>Users</Typography>
+        <Typography>Patients</Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default NavbarComponent;
