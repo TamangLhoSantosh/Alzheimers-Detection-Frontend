94694d56be654e618011b0da093633fe1f7659da
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-11-26 20:46:53 +0545

    refresh token

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -28,9 +28,24 @@ apiClient.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    console.log(error);
+  async (error) => {
+    const refreshToken = localStorage.getItem("refresh");
     if (error.response.status === 401) {
+      // If refresh token is available, attempt to refresh the token
+      if (refreshToken) {
+        try {
+          const response = await apiClient.post("/auth/refresh-token", {
+            refresh_token: refreshToken,
+          });
+          localStorage.setItem("token", response.data.access_token); // Update the token
+          error.config.headers[
+            "Authorization"
+          ] = `Bearer ${response.data.accessToken}`;
+          return apiClient.request(error.config); // Retry the failed request
+        } catch (refreshError) {
+          console.error("Token refresh failed", refreshError);
+        }
+      }
       localStorage.clear();
       window.location.replace("/login");
     }
